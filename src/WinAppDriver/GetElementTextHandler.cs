70b5d065864535310dae4c6b011f55b3d7105a3e
May Tang <maytang@kkbox.com>
2014-12-26 11:26:26 +0800

    Implement GetElementTextHandler.

---
+++ src/WinAppDriver/GetElementTextHandler.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Windows.Automation;
+namespace WinAppDriver
+{
+
+    [Route("GET", "/session/:sessionId/element/:id/text")]
+    class GetElementTextHandler : IHandler
+    {
+
+        public object Handle(Dictionary<string, string> urlParams, string body, ref Session session)
+        {
+            var element = session.GetUIElement(int.Parse(urlParams["id"]));
+            return element.Current.Name.ToString();
+        }
+    }
+}
