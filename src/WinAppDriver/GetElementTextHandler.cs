de95f316e9f48382fde5b12bd943f77f5a1101d1
May Tang <maytang@kkbox.com>
2014-12-29 18:51:27 +0800

    For more efficient to get the control pattern, change to use TryGetCurrentPattern instead of GetCurrentPropertyValue.

--- src/WinAppDriver/GetElementTextHandler.cs
+++ src/WinAppDriver/GetElementTextHandler.cs
@@ -12,9 +12,11 @@ namespace WinAppDriver
         public object Handle(Dictionary<string, string> urlParams, string body, ref Session session)
         {
             var element = session.GetUIElement(int.Parse(urlParams["id"]));
-            if (element.GetCurrentPropertyValue(AutomationElement.IsTextPatternAvailableProperty).Equals(true))
+
+            object objPattern;
+            if (element.TryGetCurrentPattern(TextPattern.Pattern, out objPattern))
             {
-                return ((TextPattern)element.GetCurrentPattern(TextPattern.Pattern)).DocumentRange.GetText(-1);
+                return ((TextPattern)objPattern).DocumentRange.GetText(-1);
             }
             return element.Current.Name;
         }
